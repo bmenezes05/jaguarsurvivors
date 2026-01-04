49045eea3f9557fdbfa32e82753ba513478baf63
bmenezes05 <bmenezes05@gmail.com>
2026-01-04 00:43:59 -0300

    feat: Redesign main menu UI with controls summary and add new map configuration and player dash files.

--- src/config/maps.config.js
+++ src/config/maps.config.js
@@ -17,11 +17,10 @@ export const mapsConfig = [
         allowedStructures: ['crate_wood'],
         music: 'cinematic1',
         waves: [
-            //{ interval: 2500, enemiesPerWave: 1, enemyTypes: ['enemy_bandido_moto', 'enemy_bandido2'], totalEnemies: 10, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 500 },
-            { interval: 2000, enemiesPerWave: 1, enemyTypes: ['enemy_favela_basico'], totalEnemies: 10, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 500 }
-            //{ interval: 1500, enemiesPerWave: 2, enemyTypes: ['enemy_bandido', 'enemy_bandido2'], totalEnemies: 30, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 500 },
-            //{ interval: 1000, enemiesPerWave: 3, enemyTypes: ['enemy_favela'], totalEnemies: 50, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 500 },
-            //{ interval: 500, enemiesPerWave: 4, enemyTypes: ['enemy_favela'], totalEnemies: 100, bossPerWave: 1, spawnDistance: 700, maxOnScreen: 500 }
+            { interval: 2000, enemiesPerWave: 1, enemyTypes: ['enemy_favela_basico'], totalEnemies: 10, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 500 },
+            { interval: 1500, enemiesPerWave: 2, enemyTypes: ['enemy_bandido', 'enemy_bandido2'], totalEnemies: 30, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 500 },
+            { interval: 1000, enemiesPerWave: 3, enemyTypes: ['enemy_favela'], totalEnemies: 50, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 500 },
+            { interval: 500, enemiesPerWave: 4, enemyTypes: ['enemy_favela'], totalEnemies: 100, bossPerWave: 1, spawnDistance: 700, maxOnScreen: 500 }
         ],
         suddenDeath: {
             interval: 10000,
@@ -30,8 +29,8 @@ export const mapsConfig = [
         boss: 'boss_bandido_moto',
         duration: 300,
         events: [
-            { time: 300, type: 'boss_spawn', key: 'boss_bandido_moto' },
-            { time: 60, type: 'final_hour' }
+            { time: 210, type: 'boss_spawn', key: 'boss_bandido_moto' },
+            { time: 45, type: 'final_hour' }
         ]
     },
     {
@@ -53,8 +52,8 @@ export const mapsConfig = [
         boss: 'boss_karl_marx',
         duration: 300,
         events: [
-            { time: 180, type: 'boss_spawn', key: 'boss_karl_marx' },
-            { time: 30, type: 'final_hour' }
+            { time: 210, type: 'boss_spawn', key: 'boss_karl_marx' },
+            { time: 45, type: 'final_hour' }
         ]
     },
     {
@@ -73,10 +72,10 @@ export const mapsConfig = [
             { interval: 1200, enemiesPerWave: 3, enemyTypes: ['enemy_petista'], totalEnemies: 60, bossPerWave: 0, spawnDistance: 700, maxOnScreen: 50 },
             { interval: 800, enemiesPerWave: 4, enemyTypes: ['enemy_partido_amarelo'], totalEnemies: 100, bossPerWave: 1, spawnDistance: 700, maxOnScreen: 50 }
         ],
-        boss: 'boss_venus',
+        boss: 'boss_tia_zap',
         duration: 300,
         events: [
-            { time: 120, type: 'boss_spawn', key: 'boss_venus' },
+            { time: 210, type: 'boss_spawn', key: 'boss_tia_zap' },
             { time: 45, type: 'final_hour' }
         ]
     }
